6d036f1e5cef41b04c1c480fc74a1c8dd282461d
iamsalmandev <salman@block360.io>
2023-03-05 15:12:17 +0500

    updates the abou me paragraph

--- components/home/about.js
+++ components/home/about.js
@@ -21,32 +21,34 @@ const About = () => {
               learning Web Development in 2017.
             </h3>
             <p>
-              My developer career started with a great emphasis on e-commerce as
-              a Shopware Front End Developer and I even worked on the
-              development of <i className="fas fa-link"></i>{' '}
-              <a
-                href="https://github.com/shopware/platform"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Shopware 6
-              </a>
-              . Later on I specialized in React and it&apos;s ecosystem. I
-              really like problem-solving in general and am addicted to the
-              feeling of solving them.
+              A MERN stack developer and Blockchain Enthusiast, with more than
+              five years of working experience. I've a good understanding of
+              typescript, javascript, ES6, React.js, React Native and a fair
+              knowledge of good coding practices i.e separation of concerns,
+              encapsulation and code splitting.
             </p>
 
             <p>
-              Besides Web Development I have a diverse work experience ranging
-              from Airfreight Logistics at the Frankfurt Airport in Germany to
-              Customer Support in Tech Companies around Europe and Brazil.
+              I have a knack for writing clean and maintainable code, and for
+              this, I religiously follow GitHub, open source community and code
+              documentations. I always write code with extensibility,
+              scalability and reusability in mind. Here is my code sample to
+              reflect my coding style:
+              https://github.com/iamsalmandev/gif-search-app
             </p>
 
             <p>
-              I&apos;m no stranger to multiculturalism and I appreciate
-              diversity as well as discovering and experiencing new cultures.
-              Loving nature and being outdoors makes my favorite pastime hiking
-              and mountain climbing.
+              I've worked with graphql, Gatsby, styled-components material-ui
+              and many other third party services like algolia, get-stream and
+              bugsnag. Strong Hands-on experience in CI/CD tools like Git and
+              bit bucket. I've also worked with many Ethereum Blockchain based
+              projects while working with block360 - a service based company in
+              the domain of blockchain.
+            </p>
+            <p>
+              Besides web development, I'm very passionate about travelling. I
+              got the cahnce to travell across the hilly terrains of Northern
+              areas of Pakistan.
             </p>
           </div>
 
